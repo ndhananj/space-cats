98823ab4224b6b6eb7e1089bdee8e4d69a7d3604
Brendan Andresen <brendan.development@pm.me>
2024-01-11 13:52:38 -0800

    added the pathlib and os module for improved file pathing

--- analytics.py
+++ analytics.py
@@ -1,5 +1,15 @@
 import requests
 import subprocess
+from pathlib import Path
+import os
+
+# Constants
+BASE_DIRECTORY = Path(__file__).resolve().parent
+
+DOWNLOADED_FILE_NAME = 'downloaded_file.pdf'
+PDF_TO_TEXT_FILE_NAME = 'pdf_to_txt.txt'
+DOWNLOADED_FILE_PATH = os.path.join(BASE_DIRECTORY, DOWNLOADED_FILE_NAME)
+PDF_TO_TEXT_FILE_PATH = os.path.join(BASE_DIRECTORY, PDF_TO_TEXT_FILE_NAME)
 
 # URL of the file to be downloaded
 url = 'https://arxiv.org/ftp/arxiv/papers/2104/2104.05314.pdf'
@@ -10,22 +20,21 @@ response = requests.get(url)
 # Check if the request was successful
 if response.status_code == 200:
     # Open a local file with 'wb' (write binary) permission.
-    with open('/home/rob/Documents/downloaded_file.pdf', 'wb') as file:
+    with open(DOWNLOADED_FILE_PATH, 'wb') as file:
         file.write(response.content)
     print("Download complete!")
 else:
     print("Failed to download the file.")
 
-
-#ebook-convert test.pdf test.txt
-result = subprocess.run(['ebook-convert', '/home/rob/Documents/downloaded_file.pdf', '/home/rob/Documents/pdf_to_txt.txt'], capture_output=True, text=True)
+# ebook-convert test.pdf test.txt
+result = subprocess.run(['ebook-convert', DOWNLOADED_FILE_PATH, PDF_TO_TEXT_FILE_PATH], capture_output=True, text=True)
 print(result.stdout)
 
 # URL from the curl command
 url = 'http://23.118.220.180:11434/api/generate'
 
 # Read the contents of the file 'test.txt'
-with open('/home/rob/Documents/pdf_to_txt.txt', 'r') as file:
+with open(PDF_TO_TEXT_FILE_PATH, 'r') as file:
     file_contents = file.read()
 
 # Prepare the JSON data payload

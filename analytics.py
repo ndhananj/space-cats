1cacd81f8fa196970d2d7a0b0aa91365de138dec
evan ricci <evanr@live.ca>
2024-01-11 22:10:14 -0800

    fixed missing imports in analytics.py added basic logging debug only for method calls adjusted prompt fixed crash if download directory didn't exist adjusted scrape from - added timestamp adjusted per_page to actually use dynamic value completely refactored scrape_from_alex added dynamic datetime

--- analytics.py
+++ analytics.py
@@ -3,14 +3,14 @@ import re
 import requests
 import subprocess
 import pprint
-
+import logging
 
 def split_contentdisposition(astring):
     # return the stuff after = sign
     value = astring.split("=")[-1]
     return re.sub(r'[^A-Za-z0-9-_\.]', '', value)
 
-def download_and_convert(download_directory, pdf_url):
+def download(pdf_url, download_directory):
 
     downloadfilename = os.path.join(download_directory,"downloaded_file.pdf")
     convertedfilename = os.path.join(download_directory,"pdf_to_text.txt")
@@ -46,7 +46,7 @@ def analyze_with_mixtral(download_directory):
     url = 'http://23.118.220.180:11434/api/generate'
     convertedfilename = os.path.join(download_directory,"pdf_to_text.txt")
     pdfsummary = os.path.join(download_directory,"pdf_summary.txt")
-    promptcommand = "Can you summarize this paper in one paragraph?"
+    promptcommand = "Can you summarize this paper in one paragraph? Only reply with the summary no other text"
     # Read the contents of the file 'test.txt'
     with open(convertedfilename, 'r') as file:
         file_contents = file.read()
@@ -75,17 +75,15 @@ def analyze_with_mixtral(download_directory):
 
 
 
-def scrape_from_alex(download_directory:str, results_per_page:str):
+def scrape_from_alex(per_page:str, date_string:str):
     import requests
-    from datetime import datetime
     import json
+    logging.debug('Starting scrape_from_alex')
 
-    #make download directory if missing
-    if not os.path.exists(download_directory):
-        os.mkdir(download_directory)
 
-    date_string = '2024-01-10'
-    per_page = results_per_page
+    # Create list to return
+    ExportList = []
+
     # Make the GET request
     response = requests.get(f"https://api.openalex.org/works?sort=cited_by_count:desc\&filter=fulltext.search:artificial%20intelligence,primary_location.is_oa:True,from_publication_date:{date_string}&per-page={per_page}")
     print(response.url)
@@ -94,19 +92,17 @@ def scrape_from_alex(download_directory:str, results_per_page:str):
     if response.status_code == 200:
         replyjson = response.json()
         meta = replyjson['meta']
-        pprint.pprint(meta)
+        # pprint.pprint(meta)
         results = replyjson['results']
         with open('latest_oa_ai_works.txt', 'w') as file:
-            for result in results: 
-                pdf_url = result['best_oa_location']['pdf_url']
-                landing_url = result["primary_location"]["landing_page_url"]
-                id = result['id']
-                print(id,pdf_url, landing_url)
-                print(id,pdf_url, landing_url, file=file)
-                if pdf_url:
-                    download_and_convert(download_directory,pdf_url)
+            for result in results:
+                print(result)
+                ExportList.append(result.get("pdf_url"))
+
+            return ExportList
                     
     else:
         print(f"Failed to fetch data: HTTP {response.status_code}")
 
+        logging.debug('Completed scrape_from_alex')
 
--- main.py
+++ main.py
@@ -1,17 +1,37 @@
 import logging
 import analytics
+from datetime import datetime
 from text_extractor import *
 from analytics import *
 
 # Set logging level to INFO, ERROR, DEBUG
 logginglevel= "DEBUG"
 downloadfolder = r"downloads"
+postprocessing = r"postprocessing"
+
+current_date = datetime.now()
+formatted_date = current_date.strftime("%Y-%m-%d")
 
 logging.basicConfig(level=logginglevel,filename='debug.log')
 
+# make necessary directories if missing
+if not os.path.exists(downloadfolder):
+    os.mkdir(downloadfolder)
+if not os.path.exists(postprocessing):
+    os.mkdir(postprocessing)
+
+#List to store urls for PDFs
+
 # Scrape from OpenAlex up to 25 papers
-analytics.scrape_from_alex(downloadfolder, "25")
+ScrapedList = analytics.scrape_from_alex("25", formatted_date)
 logging.debug('Completed AnalyticsScrapeFromAlex')
+print("returned " + str(len(ScrapedList)) + " from scrape")
+
+
+# Download from OpenAlex
+for url in ScrapedList:
+    print(url)
+    analytics.download(url, downloadfolder)
 
 # convert pdfs to text
 # analytics.download_and_convert(downloadfolder, "https://api.openalex.org/works")
